07b7679dda0636ab0424cb435c701502af565387
Angelo Vazquez <angelovazquez@Angelos-MacBook-Pro.local>
2020-11-06 11:03:43 -0600

    heroku edit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 const NODE_ENV = process.env.NODE_ENV || production;
-const DB_URL = process.env.DB_URL || 'postgresql://angelovazquez@localhost/noteful';
+const DB_URL = process.env.DATABASE_URL || 'postgresql://angelovazquez@localhost/noteful';
 
 module.exports = { PORT, NODE_ENV, DB_URL }
